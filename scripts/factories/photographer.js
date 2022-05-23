a22bd72822232ce03a1cf81bf4245b175c57dccc
angelique_rosin <angie1979.capucine@yahoo.fr>
2022-05-23 21:14:58 +0200

    refactorisation de la fonction des cartes des photographes

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,4 +1,9 @@
-// function photographerFactory(data) {
+// /**
+//  * Factory function (fonction des données des photographes)
+//  * @param {*} data 
+//  * @returns - getUserCardDOM
+//  */
+//  function photographerFactory(data) {
 //     const { name, portrait, city, country, tagline, price } = data;
 
 //     const picture = `assets/photographers/${portrait}`;
@@ -10,26 +15,24 @@
 //     function getUserCardDOM() {
 //         const article = document.createElement('article');
 //         const img = document.createElement('img');
-//         img.setAttribute('src', picture);
+//         img.setAttribute('src', picture,);
+//         img.setAttribute('alt', `Photo de ${name},`);
 //         const h2 = document.createElement('h2');
 //         h2.textContent = name;
 //         article.appendChild(img);
 //         article.appendChild(h2);
 
 //         let h3 = document.createElement ('h3');
-//         h3.textContent = city;
+//         h3.textContent = `${city}, ${country}`; 
 //         article.appendChild(h3);
 
-//         h3 = document.createElement ('h3');
-//         h3.textContent = country;
-//         article.appendChild(h3);
 
 //         const p = document.createElement ('p');
 //         p.textContent = tagline;
 //         article.appendChild(p);
 
 //         const span = document.createElement ('span');
-//         span.textContent = price;
+//         span.textContent = `${price}€/jour`;
 //         article.appendChild(span);
 
 //         return article;
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,83 @@
 //Mettre le code JavaScript lié à la page photographer.html
+/**
+ * Fonction pour récupérer les données
+ * @returns 
+ */
+async function getmedias() {
+    return fetch ('data/photographers.json')
+        .then((res) => {
+            return res.json();
+        })
+        .then((datas) => {
+            console.log(datas);
+            return datas;
+        });
+    
+}
+
+
+async function displayData(medias) {
+    const mediasSection = document.querySelector('.photograph-header');
+
+    medias.forEach((media) => {
+        const mediaModel = photographerFactory(media);
+        // console.log(mediaModel);
+        const userCardDOM = mediaModel.getUserCardDOM();
+        // console.log(userCardDOM);
+        mediasSection.appendChild(userCardDOM);
+    });
+}
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers} = await getmedias();
+    console.log(photographers);
+    
+    displayData(photographers);
+}
+
+init();
+
+/**
+ * Factory function (fonction des données des photographes)
+ * @param {*} data 
+ * @returns - getUserCardDOM
+ */
+function photographerFactory(data) {
+    const { name, portrait, city, country, tagline, } = data;
+
+    const picture = `assets/photographers/${portrait}`;
+    
+    /**
+     * Fonction de la création des cartes des photographes
+     * @returns - article
+     */
+    function getUserCardDOM() {
+        const article = document.createElement('article');
+        const img = document.createElement('img');
+        img.setAttribute('src', picture,);
+        img.setAttribute('alt', `Photo de ${name},`);
+        const h2 = document.createElement('h2');
+        h2.textContent = name;
+        article.appendChild(img);
+        article.appendChild(h2);
+
+        let h3 = document.createElement ('h3');
+        h3.textContent = `${city}, ${country}`; 
+        article.appendChild(h3);
+
+
+        const p = document.createElement ('p');
+        p.textContent = tagline;
+        article.appendChild(p);
+
+        // const span = document.createElement ('span');
+        // span.textContent = `${price}€/jour`;
+        // article.appendChild(span);
+
+        return article;
+        
+    }
+    return { name, picture, city, country, tagline, getUserCardDOM };
+}
+
