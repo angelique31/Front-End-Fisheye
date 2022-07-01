1e59b5be9adb014ed954b1aa22638d8aa290d85e
angelique_rosin <angie1979.capucine@yahoo.fr>
2022-07-01 23:03:36 +0200

    lightbox

--- src/scripts/utils/lightbox.js
+++ src/scripts/utils/lightbox.js
@@ -1,17 +1,13 @@
 const lightboxClose = document.querySelector('.lightbox_close');
-const lightboxContainer = document.querySelector('.lightbox_container');
+// const lightboxContainer = document.querySelector('.lightbox_container');
 const lightbox = document.getElementById('contact_lightbox');
 
 
- function closeModal() {
+function closeModal() {
     lightbox.style.display = 'none'; 
-    // lightboxContainer.innerHTML = "";
-
 }
 
-    
-    
- export function openModal() {
+export function openModal() {
     const lightbox = document.getElementById('contact_lightbox');
 
     if (lightbox) {
@@ -24,37 +20,31 @@ const lightbox = document.getElementById('contact_lightbox');
  */
  export function openModalById(id, medias) {
     const component = document.getElementById(id);
-    
+    // console.log(component)
     const displayLightBox = () => {
         openModal();
         const idRequest = window.location.href.split('?')[1];
         const mediaArray = component.id.split('-');
         const id = mediaArray[1];
-        
+        // console.log(id)
         const test = medias.filter(media => `${media.photographerId}` === idRequest);
-        
+        // console.log(test)
         let mediaSelected = 0;
         for (let i = 0; i < test.length; i++) {
             if (id === `${test[i].id}`) {
                 mediaSelected = i
                 break;
             };
-            
         }
         console.log("le media selectionné par le user est à la position", mediaSelected )
         
-
+        
     };
 
     if (component) {
         component.addEventListener('click', displayLightBox);
     }
-    
-     
-    
-
-
-   
+       
 }
 
 
